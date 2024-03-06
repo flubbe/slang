41a1b0c48123fccf3ddcf320d7ed275c72a6b0ef
Felix Lubbe <fe.lubbe@gmail.com>
2024-03-06 20:49:58 +0100

    Type system test template.

---
+++ test/test_type_system.cpp
@@ -0,0 +1,45 @@
+/**
+ * slang - a simple scripting language.
+ *
+ * Type system tests.
+ *
+ * \author Felix Lubbe
+ * \copyright Copyright (c) 2024
+ * \license Distributed under the MIT software license (see accompanying LICENSE.txt).
+ */
+
+#include <memory>
+
+#include <fmt/core.h>
+#include <gtest/gtest.h>
+
+#include "parser.h"
+#include "codegen.h"
+
+namespace cg = slang::codegen;
+
+namespace
+{
+
+TEST(type_system, empty)
+{
+    const std::string test_input = "";
+
+    slang::lexer lexer;
+    slang::parser parser;
+
+    lexer.set_input(test_input);
+    parser.parse(lexer);
+
+    EXPECT_TRUE(lexer.eof());
+
+    const slang::ast::block* ast = parser.get_ast();
+    EXPECT_NE(ast, nullptr);
+
+    cg::context ctx;
+    ast->generate_code(&ctx);
+
+    EXPECT_EQ(ctx.to_string().length(), 0);
+}
+
+}
