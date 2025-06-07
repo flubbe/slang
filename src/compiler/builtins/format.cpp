fc07b03576f672d834b948d8cf90db6b9272e516
Felix Lubbe <fe.lubbe@gmail.com>
2025-06-07 21:39:19 +0200

    Refactor format macro expansion.

--- src/compiler/builtins/format.cpp
+++ src/compiler/builtins/format.cpp
@@ -26,153 +26,234 @@ struct format_string_placeholder
     std::uint8_t type{0};
 };
 
-/**
- * Return format specifiers in a string.
- *
- * @param loc Location of the format string.
- * @param s The format string.
- * @returns Returns a (possibly empty) vector of format placeholders.
- */
-static std::vector<format_string_placeholder> get_format_string_placeholders(
-  token_location loc,
-  const std::string& s)
+class format_macro_expander
 {
+    /** Descriptor for the `format!` macro. */
+    const module_::macro_descriptor& desc;
+
+    /** Location of the macro invokation. */
+    token_location loc;
+
+    /** Expressions the macro operates on. */
+    const std::vector<std::unique_ptr<ast::expression>>& exprs;
+
+    /** Format string. */
+    const token& format_string;
+
+    /** Format specifiers/placeholders. */
     std::vector<format_string_placeholder> placeholders;
-    std::optional<format_string_placeholder> current_placeholder{std::nullopt};
-    std::string format_string;
 
-    for(std::size_t i = 0; i < s.length(); ++i)
+    /**
+     * Check and return the token holding the format string.
+     *
+     * @throws Throws a `codegen_error` if validation failed.
+     * @returns Returns the token holding the format string.
+     */
+    const token& get_format_token()
     {
-        if(s[i] == '{')
+        if(exprs.empty())
         {
-            if(current_placeholder.has_value())
-            {
-                throw codegen_error(
-                  loc,
-                  fmt::format(
-                    "Invalid format string '{}'.",
-                    s));
-            }
-
-            if(i + 1 < s.length() && s[i + 1] == '{')
-            {
-                placeholders.emplace_back(
-                  format_string_placeholder{i, i + 1, '{'});
-                i += 1;
-                continue;
-            }
-
-            current_placeholder = format_string_placeholder{i, 0, 0};
+            throw codegen_error(
+              loc,
+              "Cannot evaluate macro 'format!' with no arguments. Consider removing it.");
         }
-        else if(s[i] == '}')
-        {
-            if(current_placeholder.has_value())
-            {
-                current_placeholder->end = i + 1;
 
-                if(format_string.length() != 1
-                   || (format_string[0] != 'd'
-                       && format_string[0] != 'f'
-                       && format_string[0] != 's'))
+        if(!exprs[0]->is_literal())
+        {
+            throw codegen_error(
+              loc,
+              "Cannot evaluate macro 'format!': Expected <string-literal> as its first argument.");
+        }
+
+        auto* format_expr = exprs[0]->as_literal();
+        const auto& format_token = format_expr->get_token();
+        if(format_token.type != token_type::str_literal)
+        {
+            throw codegen_error(
+              format_token.location,
+              "Expected <string-literal>.");
+        }
+
+        return format_token;
+    }
+
+    /**
+     * Validate macro definition and arguments.
+     *
+     * @throws Throws a `codegen_error` if validation fails.
+     */
+    void validate()
+    {
+        // FIXME This only needs to be done when loading the macro descriptor.
+        if(desc.directives.size() != 1)
+        {
+            throw codegen_error(
+              loc,
+              fmt::format(
+                "Expected 1 directive for 'format!', got {}.",
+                desc.directives.size()));
+        }
+
+        if(desc.directives[0].first != "builtin"
+           || !desc.directives[0].second.args.empty())
+        {
+            throw codegen_error(
+              loc,
+              fmt::format(
+                "Expected 'builtin' directive for 'format' with 0 arguments, got '{}' with {} arguments.",
+                desc.directives[0].first, desc.directives[0].second.args.size()));
+        }
+    }
+
+    /**
+     * Create the format string placeholders.
+     *
+     * @throws Throws a `codegen_error` when an invalid or unsupported
+     *         format specifier was found.
+     */
+    void create_format_string_placeholders()
+    {
+        std::optional<format_string_placeholder> current_placeholder{std::nullopt};
+        std::string current_format_specifier;
+
+        for(std::size_t i = 0; i < format_string.s.length(); ++i)
+        {
+            if(format_string.s[i] == '{')
+            {
+                if(current_placeholder.has_value())
                 {
                     throw codegen_error(
                       loc,
                       fmt::format(
-                        "Unsupported format string '{}'.",
-                        format_string));
+                        "Invalid format string '{}'.",
+                        format_string.s));
                 }
-                current_placeholder->type = format_string[0];
-                format_string.clear();
 
-                placeholders.emplace_back(*current_placeholder);
-                current_placeholder = std::nullopt;
-            }
-            else
-            {
-                if(i + 1 < s.length() && s[i + 1] == '}')
+                if(i + 1 < format_string.s.length() && format_string.s[i + 1] == '{')
                 {
                     placeholders.emplace_back(
-                      format_string_placeholder{i, i + 1, '}'});
+                      format_string_placeholder{i, i + 1, '{'});
                     i += 1;
                     continue;
                 }
 
-                throw codegen_error(
-                  loc,
-                  fmt::format(
-                    "Invalid format string '{}'.",
-                    s));
+                current_placeholder = format_string_placeholder{i, 0, 0};
             }
-        }
-        else
-        {
-            if(current_placeholder.has_value())
+            else if(format_string.s[i] == '}')
             {
-                format_string += s[i];
+                if(current_placeholder.has_value())
+                {
+                    current_placeholder->end = i + 1;
+
+                    if(current_format_specifier.length() != 1
+                       || (current_format_specifier[0] != 'd'
+                           && current_format_specifier[0] != 'f'
+                           && current_format_specifier[0] != 's'))
+                    {
+                        throw codegen_error(
+                          loc,
+                          fmt::format(
+                            "Unsupported format specifier '{}'.",
+                            current_format_specifier));
+                    }
+                    current_placeholder->type = current_format_specifier[0];
+                    current_format_specifier.clear();
+
+                    placeholders.emplace_back(*current_placeholder);
+                    current_placeholder = std::nullopt;
+                }
+                else
+                {
+                    if(i + 1 < format_string.s.length() && format_string.s[i + 1] == '}')
+                    {
+                        placeholders.emplace_back(
+                          format_string_placeholder{i, i + 1, '}'});
+                        i += 1;
+                        continue;
+                    }
+
+                    throw codegen_error(
+                      loc,
+                      fmt::format(
+                        "Invalid format string '{}'.",
+                        format_string.s));
+                }
             }
+            else
+            {
+                if(current_placeholder.has_value())
+                {
+                    current_format_specifier += format_string.s[i];
+                }
+            }
+        }
+
+        if(current_placeholder.has_value())
+        {
+            throw codegen_error(
+              loc,
+              fmt::format(
+                "Invalid format string '{}'.",
+                format_string.s));
         }
     }
 
-    if(current_placeholder.has_value())
+public:
+    /** Deleted constructors. */
+    format_macro_expander() = delete;
+    format_macro_expander(const format_macro_expander&) = delete;
+    format_macro_expander(format_macro_expander&&) = delete;
+
+    /** Default destructor. */
+    ~format_macro_expander() = default;
+
+    /** Deleted assignments. */
+    format_macro_expander& operator=(const format_macro_expander&) = delete;
+    format_macro_expander& operator=(format_macro_expander&&) = delete;
+
+    /**
+     * Constructor.
+     *
+     * @param desc Descriptor for the `format!` macro.
+     * @param loc Location of the macro invokation.
+     * @param exprs Expressions the macro operates on.
+     */
+    format_macro_expander(
+      const module_::macro_descriptor& desc,
+      token_location loc,
+      const std::vector<std::unique_ptr<ast::expression>>& exprs)
+    : desc{desc}
+    , loc{loc}
+    , exprs{exprs}
+    , format_string{get_format_token()}
     {
-        throw codegen_error(
-          loc,
-          fmt::format(
-            "Invalid format string '{}'.",
-            s));
+        validate();
+        create_format_string_placeholders();
     }
 
-    return placeholders;
-}
+    /** Return the token containing the format string. */
+    [[nodiscard]]
+    const token& get_format_string() const
+    {
+        return format_string;
+    }
+
+    /** Return the format specifiers/placeholders. */
+    [[nodiscard]]
+    const std::vector<format_string_placeholder>& get_placeholders() const
+    {
+        return placeholders;
+    }
+};
 
 std::unique_ptr<ast::expression> expand_builtin_format(
   const module_::macro_descriptor& desc,
   token_location loc,
   const std::vector<std::unique_ptr<ast::expression>>& exprs)
 {
-    /*
-     * Validate macro definition.
-     * FIXME This only needs to be done when loading the macro descriptor.
-     */
-    if(desc.directives.size() != 1)
-    {
-        throw codegen_error(
-          loc,
-          fmt::format(
-            "Expected 1 directive for 'format!', got {}.",
-            desc.directives.size()));
-    }
+    format_macro_expander expander{desc, loc, exprs};
 
-    if(desc.directives[0].first != "builtin"
-       || !desc.directives[0].second.args.empty())
-    {
-        throw codegen_error(
-          loc,
-          fmt::format(
-            "Expected 'builtin' directive for 'format' with 0 arguments, got '{}' with {} arguments.",
-            desc.directives[0].first, desc.directives[0].second.args.size()));
-    }
-
-    if(exprs.empty())
-    {
-        throw codegen_error(loc, "Cannot evaluate macro 'format!' with no arguments. Consider removing it.");
-    }
-
-    if(!exprs[0]->is_literal())
-    {
-        throw codegen_error(loc, "Cannot evaluate macro 'format!': Expected <string-literal> as its first argument.");
-    }
-
-    auto* format_expr = exprs[0]->as_literal();
-    const auto& format_token = format_expr->get_token();
-    if(format_token.type != token_type::str_literal)
-    {
-        throw codegen_error(format_token.location, "Expected <string-literal>.");
-    }
-
-    // Get format placeholders.
-    auto format_placeholders = get_format_string_placeholders(format_token.location, format_token.s);
-
+    auto format_placeholders = expander.get_placeholders();
     if(format_placeholders.size() + 1 != exprs.size())
     {
         if(!format_placeholders.empty())
@@ -183,10 +264,12 @@ std::unique_ptr<ast::expression> expand_builtin_format(
         }
     }
 
+    auto format_string = expander.get_format_string();
+
     // String only.
     if(exprs.size() == 1)
     {
-        return std::make_unique<ast::literal_expression>(format_token.location, format_token);
+        return std::make_unique<ast::literal_expression>(format_string.location, format_string);
     }
 
     std::unique_ptr<ast::expression> lhs;
@@ -201,7 +284,7 @@ std::unique_ptr<ast::expression> expand_builtin_format(
         const auto& expr = exprs[expr_index];
 
         // get string fragment.
-        std::string fragment = format_token.s.substr(
+        std::string fragment = format_string.s.substr(
           last_string_fragment_end,
           fph.start - last_string_fragment_end);
         last_string_fragment_end = fph.end;
